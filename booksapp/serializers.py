41df16918945af07df8a0b481eea1b1688f5766c
lawrence <kofiarkoh0@gmail.com>
2023-10-13 17:05:11 +0000

    add endpoint to retrieve user account from db

--- booksapp/serializers.py
+++ booksapp/serializers.py
@@ -2,17 +2,21 @@ from rest_framework import serializers
 from booksapp.models import User
 from booksapp.models import Book
 
-class UserSerializer(serializers.ModelSerializer):
 
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'password', 'first_name', 'last_name', 'username', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
 
-
 class BookSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = Book
         fields = ['title', 'author', 'description', 'user']
+
+
+class RequestPasswordResetTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
--- booksapp/views.py
+++ booksapp/views.py
@@ -6,13 +6,13 @@ from rest_framework.parsers import JSONParser
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework import status
 from rest_framework import permissions
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import  login
 from knox.views import LoginView as KnoxLoginView
 from knox.auth import TokenAuthentication
-from booksapp.serializers import UserSerializer, BookSerializer
-from booksapp.models import Book
+from booksapp.serializers import UserSerializer, BookSerializer, RequestPasswordResetTokenSerializer
+from booksapp.models import Book, User
 
 # Create your views here.
 
@@ -62,3 +62,22 @@ class BooksView(APIView):
         books = Book.objects.filter(user_id=request.user.id)
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
+
+
+class RequestPasswordResetOTPView(APIView):
+
+    def post(self, request, format=None):
+
+        try:
+            data = JSONParser().parse(request)
+            serializer = RequestPasswordResetTokenSerializer(data=data)
+            if serializer.is_valid():
+                user = User.objects.get(email=serializer.validated_data['email'])
+                return Response("send the token")
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except ObjectDoesNotExist:
+            return Response({
+                'message' : 'No account exists with the provided email'
+            }, status=status.HTTP_404_NOT_FOUND)
